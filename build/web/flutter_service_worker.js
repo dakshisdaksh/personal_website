4c6850682a91cf382fd0fec9b7efe3a670de78bc
Shrish Dwivedi <72087189+shhdwi@users.noreply.github.com>
2022-02-27 23:13:25 +0530

    added CV

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "3edd76865898b27b621bca000e1b1c6c",
+  "assets/AssetManifest.json": "f3ee79746da0791faabdba7d26281386",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/lib/icons/github_2x.png": "6401c06a4431eb5619083e6a1783ed63",
@@ -17,23 +17,34 @@ const RESOURCES = {
 "assets/lib/images/gdsc%2520banner.jpg": "a9e53dc4abe44592333fa572ccc63242",
 "assets/lib/images/graph.jpg": "e98d830a24636f825bae8b663a3c5c8c",
 "assets/lib/images/hydrogen.jpg": "2f52ba6e9bf4136082e17a1788a5183b",
+"assets/lib/images/img1-left.png": "fb5ecb57810989235ffa56574fc9277f",
+"assets/lib/images/img1-right.png": "1f5d9a5cdac6cdf4fb94d919856c8611",
+"assets/lib/images/img2-left.png": "0c72ffc26d4b25df5c4e65e434a6abe4",
+"assets/lib/images/img2-right.png": "5a162e5712e7170830be0aed24cc5b48",
 "assets/lib/images/insta.png": "6877fc99e2f36909c43ed69a833e93eb",
 "assets/lib/images/instag.png": "77a11af568eb6682ee7cd58ad99a3ac1",
 "assets/lib/images/instagg.png": "5c6fd3d7e01159fe3b2df3cfdc8d64fb",
 "assets/lib/images/instagram.png": "aa0c51a2ba698c737479f0bb20025c6b",
 "assets/lib/images/inta.png": "2a10c2344a8291bbde864ffbdbde9895",
 "assets/lib/images/loader.json": "a59f97db2fae4bf3baf0ecf4d54026e5",
+"assets/lib/images/ok.png": "96a164931819b47031ab49bc9484a748",
+"assets/lib/images/okok.png": "e5c03d35a3b2d0dd3e3e6197efdf5b69",
 "assets/lib/images/shrish.jpeg": "4f681809feecb6707670fc97ee9906c6",
-"assets/NOTICES": "7ded6495c3e75b84105d52033ea72301",
+"assets/lib/images/shrish.png": "c6218c8e5dfc10cdd534c25afef6bb43",
+"assets/NOTICES": "d930321909dd185861a5566c90505969",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f2e3a06a67d4ca0dc02c313f7991454b",
-"/": "f2e3a06a67d4ca0dc02c313f7991454b",
-"main.dart.js": "46d713626d5f73b3b46b65479e825a3d",
+"index.html": "551dc71fb7cccf3329d1ece274a1be0a",
+"/": "551dc71fb7cccf3329d1ece274a1be0a",
+"main.dart.js": "adcaa5976a171916c371e4f7fd1b40cb",
 "manifest.json": "3abcd20a2983d79ae79494a2ce385a7a",
-"version.json": "26135e4be13343012ada35bf6692fcf3"
+"version.json": "03acefc4795e8573b194262cd3a4419f"
 };
 
 // The application shell files that are downloaded before a service worker can
